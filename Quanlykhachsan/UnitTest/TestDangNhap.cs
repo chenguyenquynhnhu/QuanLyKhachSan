6d8214a9a897268bf9d7a987724e0e3853a82534
quynhnhu <chenguyenquynhnhu@gmail.com>
2019-04-04 19:42:33 +0700

    Tao unit test

--- Quanlykhachsan/UnitTest/TestDangNhap.cs
+++ Quanlykhachsan/UnitTest/TestDangNhap.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using Quanlykhachsan;
+//using Quanlykhachsan.Lopdoituong;
 
 namespace UnitTest
 {
     [TestClass]
     public class TestDangNhap
     {
-        private LNhanvien ten;
+        private Lnhanvien ten;
+
         [TestMethod]
         public void TestMethod1()
         {
