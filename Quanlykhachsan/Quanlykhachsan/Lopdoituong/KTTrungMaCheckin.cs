fba3f4af9fd648bab955798d6b7168e7ebf7ed36
quynhnhu <1554050064nhu@ou.edu.vn>
2019-04-09 16:58:31 +0700

    Unittest trung ma checkin

---
+++ Quanlykhachsan/Quanlykhachsan/Lopdoituong/KTTrungMaCheckin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using Quanlykhachsan.Lopdoituong;
+using System.Data;
+
+namespace Quanlykhachsan.Lopdoituong
+{
+    public class KTTrungMaCheckin
+    {
+        public static bool kt_trungmacheckin(string macheckin)
+        {
+            try
+            {
+
+                DataSet ds1 = new DataSet();
+
+
+                ketnoi.ThietlapketNoi();
+                laydulieu dl = new laydulieu();
+                ds1 = dl.getdata("insert into checkin(macheckin)values('" + macheckin + "')");
+                return false;
+
+            }
+            catch { return true; }
+        }
+    }
+}
---
+++ Quanlykhachsan/UnitTest/TestTrungMaCheckin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using Quanlykhachsan;
+using Quanlykhachsan.Lopdoituong;
+using System.Windows;
+
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestTrungMaCheckin
+    {
+        [TestMethod]
+        public void test_trungmacheckin()
+        {
+            bool actual = KTTrungMaCheckin.kt_trungmacheckin("CK01");
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
