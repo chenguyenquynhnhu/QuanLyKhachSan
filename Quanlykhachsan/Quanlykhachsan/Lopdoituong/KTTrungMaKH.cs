5bedf080b6f10963038fbc2ff6650b15d289f3b4
quynhnhu <1554050064nhu@ou.edu.vn>
2019-04-09 16:47:50 +0700

    Unittest test trung ma KH

---
+++ Quanlykhachsan/Quanlykhachsan/Lopdoituong/KTTrungMaKH.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using Quanlykhachsan.Lopdoituong;
+using System.Data;
+
+namespace Quanlykhachsan.Lopdoituong
+{
+    public class KTTrungMaNV
+    {
+        public static bool kt_trungmaKH(string maKH)
+        {
+            try
+            {
+
+                DataSet ds1 = new DataSet();
+
+
+                ketnoi.ThietlapketNoi();
+                laydulieu dl = new laydulieu();
+                ds1 = dl.getdata("insert into khachhang(makhach)values('" + maKH + "')");
+                return false;
+
+            }
+            catch { return true; }
+        }
+    }
+}
---
+++ Quanlykhachsan/UnitTest/TestTrungMaKH.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using Quanlykhachsan;
+using Quanlykhachsan.Lopdoituong;
+using System.Windows;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestTrungMaKH
+    {
+        [TestMethod]
+        public void Test_trungmaKH()
+        {
+            bool actual = KTTrungMaNV.kt_trungmaKH("KH001");
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
