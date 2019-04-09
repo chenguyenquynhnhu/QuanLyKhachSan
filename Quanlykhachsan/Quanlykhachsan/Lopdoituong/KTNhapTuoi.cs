d6d2b6f014cdbb819c9557138afbbbcaa2df91ec
quynhnhu <1554050064nhu@ou.edu.vn>
2019-04-09 17:47:01 +0700

    Unittest Nhap tuoi khong phai dang so

---
+++ Quanlykhachsan/Quanlykhachsan/Lopdoituong/KTNhapTuoi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using Quanlykhachsan.Lopdoituong;
+using System.Data;
+
+namespace Quanlykhachsan.Lopdoituong
+{
+    public class KTNhapTuoi
+    {
+        public static bool kt_nhaptuoi(string tuoi)
+        {
+            try
+            {
+
+                DataSet ds1 = new DataSet();
+
+
+                ketnoi.ThietlapketNoi();
+                laydulieu dl = new laydulieu();
+                ds1 = dl.getdata("insert into khachhang(tuoi)values('" + tuoi + "')");
+                return false;
+
+            }
+            catch { return true; }
+        }
+    }
+}
