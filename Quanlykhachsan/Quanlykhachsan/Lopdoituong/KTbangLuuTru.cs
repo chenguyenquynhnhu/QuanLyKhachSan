8a950038cf2bc4d6d52f0e93634f67d46ceb88b0
TranHoangTruc <1554050108truc@ou.edu.vn>
2019-04-13 15:20:26 +0700

    Unittest sua thong tin luu tru nhap so phong khong phai dang so

---
+++ Quanlykhachsan/Quanlykhachsan/Lopdoituong/KTbangLuuTru.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using Quanlykhachsan.Lopdoituong;
+using System.Data;
+using System.Windows.Forms;
+namespace Quanlykhachsan.Lopdoituong
+{
+    public class KTbangLuuTru
+    {
+        public static bool kt_nhapSoPhong(string SoPhong)
+        {
+            try
+            {
+                if (SoPhong == "")
+                {
+                    return true;
+                }
+                DataSet ds1 = new DataSet();
+
+
+                ketnoi.ThietlapketNoi();
+                laydulieu dl = new laydulieu();
+                ds1 = dl.getdata("insert into luutru(phongdon)values('" + SoPhong + "')");
+                return false;
+
+            }
+            catch { return true; }
+        }
+    }
+}
---
+++ Quanlykhachsan/UnitTest/TestbangLuuTru.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using Quanlykhachsan;
+using Quanlykhachsan.Lopdoituong;
+using System.Windows;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestbangLuuTru
+    {
+        [TestMethod]
+        public void Test_NhapSoPhongKhongPhaiDangSo()
+        {
+            bool actual = KTbangLuuTru.kt_nhapSoPhong("hai");
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
